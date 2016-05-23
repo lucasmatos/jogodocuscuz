04f8993b3bd2b0bf7aa5c395a333a6f8eeb10684
lucasmatos <lucas_matos_15@hotmail.com>
2016-05-23 16:31:35 -0300

    primeiro commit

---
+++ JogoDoCuscuz/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace JogoDoCuscuz.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ JogoDoCuscuz/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JogoDoCuscuz.Startup))]
+namespace JogoDoCuscuz
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
